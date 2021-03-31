3720d0becc964a59abee7b9e96349af2800d02b9
Obuibo11 <nathan@desmedt.me>
2021-03-31 17:40:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/jeucanon/BalleCanon.kt
@@ -0,0 +1,81 @@
+package com.example.jeucanon
+
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PointF
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+class BalleCanon (var view: CanonView, val obstacle: Obstacle, val cible: Cible) {
+    var canonball = PointF()
+    var canonballVitesse = 0f
+    var canonballVitesseX = 0f
+    var canonballVitesseY = 0f
+    var canonballOnScreen = true
+    var canonballRadius = 0f
+    var canonballPaint = Paint()
+
+
+    init {
+        canonballPaint.color = Color.RED
+    }
+
+    fun launch(angles: Double) {
+        val angle=angles- Math.PI/2
+        canonball.x = view.screenWidth/2f
+        canonball.y = view.screenHeight -canonballRadius
+        canonballVitesseX=(canonballVitesse*Math.cos(angle)).toFloat()
+        canonballVitesseY=(canonballVitesse*Math.sin(angle)).toFloat()
+        canonballOnScreen = true
+
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    fun update(interval: Double) {
+        if (canonballOnScreen) {
+            canonball.x += (interval * canonballVitesseX).toFloat()
+            canonball.y += (interval * canonballVitesseY).toFloat()
+
+            /* Vérifions si la balle touche l'obstacle ou pas */
+            if (canonball.x + canonballRadius > obstacle.obstacle.left &&
+                canonball.y + canonballRadius > obstacle.obstacle.top &&
+                canonball.y - canonballRadius < obstacle.obstacle.bottom) {
+                canonballVitesseX *= -1
+                canonball.offset((5*canonballVitesseX*interval).toFloat(), 0f)
+                view.reduceTimeLeft()
+                view.playObstacleSound()
+            }
+
+            // Si elle sorte de l'écran
+            else if (canonball.x + canonballRadius > view.screenWidth
+                || canonball.x - canonballRadius < 0) {
+                canonballOnScreen = false
+            }
+            else if (canonball.y + canonballRadius > view.screenHeight
+                || canonball.y - canonballRadius < 0) {
+                canonballOnScreen = false
+            }
+            else if (canonball.x+canonballRadius>cible.cible.left
+                && canonball.y+canonballRadius>cible.cible.top
+                && canonball.y-canonballRadius<cible.cible.bottom) {
+                if (canonball.x + canonballRadius > cible.cible.left
+                    && canonball.y + canonballRadius > cible.cible.top
+                    && canonball.y - canonballRadius < cible.cible.bottom) {
+                    cible.detectChoc(this)
+                }
+
+            }
+
+        }
+    }
+
+    fun draw(canvas: Canvas) {
+        canvas.drawCircle(canonball.x, canonball.y, canonballRadius,
+            canonballPaint)
+    }
+    fun resetCanonBall() {
+        canonballOnScreen = false
+    }
+}
---
+++ app/src/main/java/com/example/jeucanon/Canon.kt
@@ -0,0 +1,29 @@
+package com.example.jeucanon
+
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.PointF
+
+class Canon (var canonBaseRadius: Float, var canonLongueur: Float, hauteur: Float, var largeur: Float, val view: CanonView) {
+    val canonPaint = Paint()
+    var finCanon = PointF(view.screenWidth/2+largeur,
+            view.screenHeight-canonLongueur)
+
+    fun draw(canvas: Canvas) {
+        canonPaint.strokeWidth = largeur * 1.5f
+        canvas.drawLine(view.screenWidth/2, view.screenHeight, finCanon.x,finCanon.y, canonPaint)
+        canvas.drawCircle(view.screenWidth/2, view.screenHeight, canonBaseRadius,
+            canonPaint)
+    }
+
+    fun setFinCanon(hauteur: Float) {
+        finCanon.set(PointF(view.screenWidth/2+hauteur,
+                view.screenHeight-canonLongueur))
+    }
+    fun align( angles: Double) {
+        val angle=angles
+        finCanon.x = (canonLongueur * Math.sin(angle)+view.screenWidth/2).toFloat()
+        finCanon.y = (-canonLongueur * Math.cos(angle)
+                + view.screenHeight ).toFloat()
+    }
+}
